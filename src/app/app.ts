cb5dfc83fe904b9e5a03480495942b2a622f6e1e
osama sameh <oo215881@gmail.com>
2026-06-17 02:15:09 +0300

    frist command

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./componants/navbar/navbar";
+import { Hero } from "./componants/hero/hero";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
