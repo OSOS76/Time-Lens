d2afe1ea8682fdd9d72e941a4ecf7bcb5d0313ad
osama sameh <oo215881@gmail.com>
2026-06-18 18:05:11 +0300

    features sec

--- src/app/componants/home/home.ts
+++ src/app/componants/home/home.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { Navbar } from "../navbar/navbar";
 import { Hero } from '../hero/hero';
 import { RouterOutlet } from '@angular/router';
+import { Features } from '../features/features';
 // import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar,Hero,RouterOutlet],
+  imports: [Navbar,Hero,Features,RouterOutlet],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
