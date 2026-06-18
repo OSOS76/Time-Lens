c3e27831f2835c89e120ba228b18f81cdf77082a
osama sameh <oo215881@gmail.com>
2026-06-18 23:15:16 +0300

    chalanges section

---
+++ src/app/componants/challenges/challenges.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-challenges',
+  imports: [],
+  templateUrl: './challenges.html',
+  styleUrl: './challenges.scss',
+})
+export class Challenges {}
--- src/app/componants/home/home.ts
+++ src/app/componants/home/home.ts
@@ -3,12 +3,13 @@ import { Navbar } from "../navbar/navbar";
 import { Hero } from '../hero/hero';
 import { RouterOutlet } from '@angular/router';
 import { Features } from '../features/features';
+import { Challenges } from '../challenges/challenges';
 // import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar,Hero,Features,RouterOutlet],
+  imports: [Navbar,Hero,Features,Challenges,RouterOutlet],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
