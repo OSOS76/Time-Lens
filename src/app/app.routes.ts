39a33928c145263b005de42a662fc337379a4d73
osama sameh <oo215881@gmail.com>
2026-06-30 00:52:37 +0300

    final5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,55 @@
 import { Routes } from '@angular/router';
-import { Home } from './componants/home/home';
-import { Bookvr } from './componants/bookvr/bookvr';
-import { Login } from './componants/login/login';
-import { Signup } from './componants/signup/signup';
 import { authGuard } from '../guards/auth-guard';
 
 export const routes: Routes = [
-  { path: 'home', component: Home },
-  // { path: 'bookvr', component: Bookvr },
-  {
-    path:'bookvr',
-    component:Bookvr,
-    canActivate:[authGuard]
-  },
-  { path: 'login', component: Login },
-  { path: 'signup', component: Signup },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', redirectTo: 'home' },
+
+{
+path:'home',
+
+loadComponent:()=>
+import('./componants/home/home')
+.then(m=>m.Home)
+
+},
+
+{
+path:'bookvr',
+
+loadComponent:()=>
+import('./componants/bookvr/bookvr')
+.then(m=>m.Bookvr),
+
+canActivate:[authGuard]
+
+},
+
+{
+path:'login',
+
+loadComponent:()=>
+import('./componants/login/login')
+.then(m=>m.Login)
+
+},
+
+{
+path:'signup',
+
+loadComponent:()=>
+import('./componants/signup/signup')
+.then(m=>m.Signup)
+
+},
+
+{
+path:'',
+redirectTo:'home',
+pathMatch:'full'
+},
+
+{
+path:'**',
+redirectTo:'home'
+}
+
 ];
